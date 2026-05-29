03322d9537e5299c4e710d0ca1637688b11bf5d2
kartikpro9599-hash <kartikpro9599@gmail.com>
2026-05-29 15:47:01 +0530

    removed backend running msg from ui

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,26 +1,10 @@
 import Login from "../src/pages/Login";
-import axios from 'axios';
-import { useEffect, useState } from 'react';
 
 function App() {
   const backendUrl = 'http://localhost:5000';
 
-  const [backendData,setBackendData] = useState("");
-  useEffect(() => {
-
-    axios.get(`${backendUrl}/patients`)
-      .then((res) => {
-        console.log(res.data);
-        setBackendData(res.data);
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-
-  }, []);
-
   return (
-    <Login back={backendData} url={`${backendUrl}/patients/submit-data`} />
+    <Login url={`${backendUrl}/patients/submit-data`} />
   )
 }
 
