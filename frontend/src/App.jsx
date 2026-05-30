5125fd75afa41e863da1426681a5efd0ac9731b3
kartikpro9599-hash <kartikpro9599@gmail.com>
2026-05-30 14:26:49 +0530

    add paths feature and use pages as per route

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,21 @@
-import Login from "../src/pages/Login";
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
 function App() {
   const backendUrl = 'http://localhost:5000';
-
   return (
-    <Login url={`${backendUrl}/patients/submit-data`} />
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path="/"
+          element={<Home/>}
+        />
+        <Route
+          path="/login"
+          element={<Login url={`${backendUrl}/patients/submit-data`} />}
+        />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
