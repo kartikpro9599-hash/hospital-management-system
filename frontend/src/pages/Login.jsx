1b06b6f76cf92aa9878a9881062c84830c2af70c
kartikpro9599-hash <kartikpro9599@gmail.com>
2026-06-01 19:01:30 +0530

    Add route guard to prevent direct access to login page

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,8 +1,20 @@
-import UserLogin from "../components/LoginCard";
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import UserLogin from "../components/LoginCard"
 
-function Login(props) { 
-    return (
-        <UserLogin url={props.url} />
-    );
+function Login(props) {
+    const navigate = useNavigate();
+
+    useEffect(() => {
+    //remove direct access of /login route for role set 
+        const loginType = localStorage.getItem("loginType");
+
+        if (!loginType) {
+            navigate("/", { replace: true });
+        }
+    }, [navigate]);
+
+    return <UserLogin url={props.url} />;
 }
+
 export default Login;
