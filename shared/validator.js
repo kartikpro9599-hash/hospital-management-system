09b8f9eff7e26e90781df0c3643ec55de535c0f1
kartikpro9599-hash <kartikpro9599@gmail.com>
2026-06-22 15:15:56 +0530

    make an shared folder for validation logic for both frontend and backend because frontend will prevent the bad request for accidentally pressed submit btn if its not a valid credential and improve some logic for email whitelisting.js for more accurate  eemail

---
+++ shared/validator.js
@@ -0,0 +1,65 @@
+// learn validators from
+//https://youtu.be/ogVEqykJs_o?si=lyaemXGos28pyowP and https://youtu.be/T-GqjaS19SQ?si=kRCTWE846d6fMAc4 these are great tutorial and read the documentation of zod
+//https://regexr.com/ for regex
+//zod.dev/api#enum
+
+import * as z from "zod";
+
+export const createAccountValidator = z
+  .object({
+    fName: z
+      .string()
+      .trim()
+      .min(2, { message: "please enter the correct name" })
+      .max(20, { message: "First name is too long" })
+      .regex(/^[A-Za-z]+$/, { message: "Name can only contain letters" }),
+
+    lName: z
+      .string()
+      .trim()
+      .min(2, { message: "please enter the correct last name" })
+      .max(20, { message: "last name is too long" })
+      .regex(/^[A-Za-z]+$/, { message: "Name can only contain letters" }),
+
+    username: z
+      .string()
+      .trim()
+      .min(2, { message: "username is too short" })
+      .max(20, { message: "username is too long" })
+      .regex(/^[A-Za-z0-9_]+$/, {
+        message: "Username can contain only letters, numbers and _",
+      }),
+
+    age: z
+      .number({ message: "enter the correct age" })
+      .int({ message: "age must be a number" })
+      .min(1, { message: "please enter the currect age" })
+      .max(150, { message: "your are too old for registratiom" }), //i dont know what msg should i pass so i make this msg funny
+
+    gender: z.enum(["male", "female", "transgender"]),
+
+    phoneNo: z
+      .string()
+      .trim()
+      .regex(/^[6-9][0-9]{9}$/, {
+        message: "Invalid phone number or do not use space",
+      }),
+
+    email: z.email({ message: "please enter the correct email" }),
+
+    password: z
+      .string()
+      .min(8, { message: "password must contain at least 8 letters" })
+      .regex(
+        /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[A-Za-z\d@$!%*?&^#()_+\-=[\]{};':"\\|,.<>/?]{8,}$/,
+        { message: "Password must contain uppercase, lowercase and a number" },
+      ),
+
+    cnfPassword: z
+      .string()
+      .min(8, { message: "password must contain at least 8 letters" }),
+  })
+  .refine((data) => data.password === data.cnfPassword, {
+    message: "Passwords does not match",
+    path: ["cnfPassword"],
+  });
---
+++ shared/whitelisting_email.domain.js
@@ -0,0 +1,30 @@
+/* learn whitelisting from youtube
+list from https://email-verify.my-addr.com/list-of-most-popular-email-domains.php
+choose the best domain as per your need
+i choose overall frontend + backend both form validation so if user accidentally input false things then frontend will reject them and server will save all the their energy like bandwith ram usage etc.
+*/
+const onlyWhiteListedEmail = new Set([
+  "gmail.com",
+  "yahoo.com",
+  "outlook.com",
+  "hotmail.com",
+  "icloud.com",
+  "live.com",
+  "proton.com",
+  "proton.me",
+  "mail.com",
+]);
+
+function isWhitelisted(email) {
+  // search for how to split email and chaining
+  if (!email || typeof email !== "string") return false;
+
+  const email_domain = email.trim().toLowerCase().split("@")[1]?.trim();
+
+  if (!email_domain) return false;
+  // .has prototype returns boolean so on the basis of this we will validate the form valid email
+
+  return onlyWhiteListedEmail.has(email_domain);
+}
+
+export default isWhitelisted;
