8eb05bd1894f33285ad6102d21d53004d0e9f31e
kartikpro9599-hash <kartikpro9599@gmail.com>
2026-06-25 15:54:01 +0530

    now used login validator because we can see that when registering the form we strict check for patterns and also some kind of pattern used to save the db so pattern are same if pattern not met then already tackle the the username and password is wrong this is 100% give surity that user have forgot the username or password or he doing the spam

--- shared/validator.js
+++ shared/validator.js
@@ -36,7 +36,9 @@ export const createAccountValidator = z
       .min(1, { message: "please enter the currect age" })
       .max(150, { message: "your are too old for registratiom" }), //i dont know what msg should i pass so i make this msg funny
 
-    gender: z.enum(["male", "female", "transgender"]),
+    gender: z.enum(["male", "female", "transgender"], {
+      message: "please choose your gender",
+    }),
 
     phoneNo: z
       .string()
@@ -63,3 +65,24 @@ export const createAccountValidator = z
     message: "Passwords does not match",
     path: ["cnfPassword"],
   });
+
+export const loginAccountValidator = z.object({
+  username: z
+    .string()
+    .trim()
+    .min(2, { message: "username is too short" })
+    .max(20, { message: "username is too long" })
+    .regex(/^[A-Za-z0-9_]+$/, {
+      message: "username or Password is incorrrect",
+    }),
+  password: z
+    .string()
+    .min(8, { message: "password must contain at least 8 letters" })
+    .regex(
+      /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[A-Za-z\d@$!%*?&^#()_+\-=[\]{};':"\\|,.<>/?]{8,}$/,
+      { message: "username or Password is incorrrect" },
+    ),
+  loginType: z.enum(["patient", "doctor", "admin"], {
+    message: "Please do not change the Local Storage",
+  }),
+});
