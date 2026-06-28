59d9144872739e4f2b0936367a3ecd67ea6c66d3
kartikpro9599-hash <kartikpro9599@gmail.com>
2026-06-28 14:48:28 +0530

    change the protectroute logic for ui improvemnt and check that user is genuine or not

--- frontend/src/routes/ProtectedRoute.jsx
+++ frontend/src/routes/ProtectedRoute.jsx
@@ -1,10 +1,46 @@
 import { Navigate } from "react-router-dom";
+import api from "../api/api.js";
+import { useEffect, useState } from "react";
+import { HashLoader } from "react-spinners";
 
 function ProtectedRoute({ children }) {
-    const isLogIn = localStorage.getItem("isLoggedIn") === "true";
-    if (!isLogIn) {
-        return <Navigate to="/" replace />;
+    const [loading, setLoading] = useState(true);
+    const [auth, setAuth] = useState(false);
+
+    useEffect(() => {
+        const checkingUser = async () => {
+            try {
+                const req = await api.get("/api/me");
+                if (req.data.success) {
+                    setAuth(true)
+                } else {
+                    setAuth(false);
+                }
+            } catch (error) {
+                console.log("user is not signed in", error);
+                setAuth(false);
+            } finally {
+                setLoading(false);
+            }
+        };
+        checkingUser();
+    }, [])
+    if (loading) {
+        //search for spinner animation
+        return (<div style={{
+            height: "100vh",
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center"
+        }}>
+            <HashLoader />
+        </div>
+        );
     }
+    if (!auth) {
+        <Navigate to="/" replace />
+    }
+
     return children;
 }
 export default ProtectedRoute;
