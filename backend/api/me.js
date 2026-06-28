947cad85a571270240f381b18fdd5b202c8c2e26
kartikpro9599-hash <kartikpro9599@gmail.com>
2026-06-28 14:38:56 +0530

    add new api name findMe on route /me for authorization purposess

---
+++ backend/api/me.js
@@ -0,0 +1,36 @@
+import express from "express";
+import db from "../db.js";
+import auth from "../middleware/authorised.js";
+
+const findMe = express.Router();
+
+findMe.get("/me", auth, async (req, res) => {
+  try {
+    const user = await db.query(
+      "SELECT fName, lName from patient where username = $1",
+      [req.user.username],
+    );
+    if (user.rowCount === 0) {
+      return res.status(200).json({
+        success: false,
+        message: "user not found",
+      });
+    }
+    return res.status(200).json({
+      success: true,
+      message: "User have valid credentials",
+      user: {
+        fName: user.rows[0].fname,
+        lName: user.rows[0].lName,
+      },
+    });
+  } catch (error) {
+    console.log("error from findMe route : ", error);
+    return res.status(500).json({
+      success: false,
+      message: "failed due to an internal server error",
+    });
+  }
+});
+
+export default findMe;
