603c41981b88c9a4b39b24cee59e45bd78f0b157
kartikpro9599-hash <kartikpro9599@gmail.com>
2026-07-28 13:42:39 +0530

    add new variables like token for storing it on memmory for not getting the stolen token is accesstoken

--- frontend/src/routes/AuthProvider.jsx
+++ frontend/src/routes/AuthProvider.jsx
@@ -4,19 +4,21 @@ import { AuthContext } from "./AuthContext.js"
 
 import api from '../api/api';
 
-export const AuthProvider = ({ children }) => {
+function AuthProvider({ children }) {
     const [user, setUser] = useState(null);
     const [loading, setLoading] = useState(true);
-
+    const [token, setToken] = useState(null);
 
     useEffect(() => {
         const verifyUser = async () => {
             try {
                 const request = await api.get('/api/me');
-                setUser(request.data.user);
+                setUser(request.data?.user);
+                setToken(request.data.token);
             } catch (error) {
                 console.log("error in auth :", error);
                 setUser(null);
+                setToken(null);
             } finally {
                 setLoading(false);
             }
@@ -27,11 +29,11 @@ export const AuthProvider = ({ children }) => {
     const login = (userData) => {
         setUser(userData);
     }
-
     return (
-        <AuthContext.Provider value={{ user, loading, login }}>
+        <AuthContext.Provider value={{ user, setUser, token, setToken, loading, login }}>
             {children}
         </AuthContext.Provider>
     );
 };
 
+export default AuthProvider
