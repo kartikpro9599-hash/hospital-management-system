6e7df9be8c179ee2b0f372d6961b479aa327eb1c
kartikpro9599-hash <kartikpro9599@gmail.com>
2026-07-28 13:47:54 +0530

    successfully migrated from localstorage to memory

--- frontend/src/components/sub-components/LogoutBtn.sub.jsx
+++ frontend/src/components/sub-components/LogoutBtn.sub.jsx
@@ -1,14 +1,18 @@
 import { useNavigate } from "react-router-dom";
+import { useAuth } from "../../routes/useAuth.js";
+import api from "../../api/api.js"
 
 function LogOut() {
     const navigate = useNavigate();
+    const { setUser, setToken } = useAuth();
+    async function handleLogOut() {
 
-    function handleLogOut() {
-        localStorage.removeItem("isLoggedIn");
-        localStorage.removeItem("username");
-        localStorage.removeItem("loginType");
-        localStorage.removeItem("createPatient");
-        navigate("/");
+        const req = await api.get('/api/logout');
+        if (req.data.success) {
+            setUser(null);
+            setToken(null);
+            navigate("/");
+        }
     }
     return (
         < button type="button" name="logout" id="logout" onClick={handleLogOut} > LogOut</button >
