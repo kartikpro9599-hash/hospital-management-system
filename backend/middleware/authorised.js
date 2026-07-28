dfd714261b0e597da3fb9532f25eb6a5ca5cf94a
kartikpro9599-hash <kartikpro9599@gmail.com>
2026-07-28 13:39:49 +0530

    fix auth middleware for auth purposes in this i changed only check for token validity to also check token validity and is this token is have in db or not if not so not authorised

--- backend/middleware/authorised.js
+++ backend/middleware/authorised.js
@@ -1,6 +1,7 @@
 import jwt from "jsonwebtoken";
+import db from "../db.js";
 
-function auth(req, res, next) {
+async function auth(req, res, next) {
   const refreshToken = req.cookies.token;
   if (!refreshToken) {
     return res.status(401).json({
@@ -9,19 +10,35 @@ function auth(req, res, next) {
     });
   }
   try {
-    const accessTokenValidation = req.body.token;
-    if (accessTokenValidation) {
-      const verifyAccess = jwt.verify(refreshToken, process.env.JWTSIGN_ACCESS);
+    if (req.body?.token) {
+      const accessTokenValidation = req.body.token;
 
+      const verifyAccess = jwt.verify(
+        accessTokenValidation,
+        process.env.JWTSIGN_ACCESS,
+      );
+      //currently my frontend does not send access token
       if (verifyAccess) {
-        return res.status(200).json({
-          success: true,
-          message: "Valid user",
-        });
+        req.user = verifyAccess;
+        next();
       }
     }
-
     const decode = jwt.verify(refreshToken, process.env.JWTSIGN_REFRESH);
+    console.log(decode);
+    const result = await db.query(
+      "SELECT * from refreshtokens where userid = $1 AND usertype = $2",
+      [decode.userid, decode.userType],
+    );
+    console.log(result);
+    if (result.rowCount === 0 || result.rows[0].token !== refreshToken) {
+      return res
+        .clearCookie("token", { httpOnly: true, sameSite: "lax" })
+        .status(401)
+        .json({
+          success: false,
+          message: "you have been logout someone login in other device",
+        });
+    }
     req.user = decode;
     next();
   } catch (error) {
