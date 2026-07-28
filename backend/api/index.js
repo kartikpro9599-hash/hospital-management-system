5d215f0cc0ef8449709004a611ec65a9a832757b
kartikpro9599-hash <kartikpro9599@gmail.com>
2026-07-28 13:36:29 +0530

    add new route /logout  and fix /me route for not generating refreshtoken

--- backend/api/index.js
+++ backend/api/index.js
@@ -3,11 +3,13 @@ import express from "express";
 import findMe from "./me.js";
 import loginRoute from "./auth/login.js";
 import createAccountRoute from "./auth/createAccount.js";
+import logoutRoute from "./logout.js";
 
 const apiRoutes = express.Router();
 
 apiRoutes.use(loginRoute);
 apiRoutes.use(createAccountRoute);
 apiRoutes.use(findMe);
+apiRoutes.use(logoutRoute);
 
 export default apiRoutes;
--- backend/api/me.js
+++ backend/api/me.js
@@ -1,6 +1,7 @@
 import express from "express";
 import db from "../db.js";
 import auth from "../middleware/authorised.js";
+import jwt from "jsonwebtoken";
 
 import { globalRateLimiter } from "../middleware/rateLimiter.js";
 
@@ -8,22 +9,30 @@ const findMe = express.Router();
 
 findMe.get("/me", globalRateLimiter, auth, async (req, res) => {
   try {
-    const user = await db.query(
-      "SELECT fName, lName from patient where username = $1",
-      [req.user.username],
-    );
-    if (user.rowCount === 0) {
+    const data = await db.query("SELECT * from patients where username = $1", [
+      req.user.username,
+    ]);
+    if (data.rowCount === 0) {
       return res.status(404).json({
         success: false,
         message: "user not found",
       });
     }
+    const accessToken = jwt.sign(
+      { username: data.rows[0].username },
+      process.env.JWTSIGN_ACCESS,
+      {
+        expiresIn: "15m",
+      },
+    );
     return res.status(200).json({
       success: true,
       message: "User have valid credentials",
+      token: accessToken,
       user: {
-        fName: user.rows[0].fname,
-        lName: user.rows[0].lName,
+        fName: data.rows[0].fname,
+        lName: data.rows[0].lName,
+        username: data.rows[0].username,
       },
     });
   } catch (error) {
